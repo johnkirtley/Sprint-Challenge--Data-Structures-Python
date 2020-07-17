25f47ab6250ef68b9d26b9e58c0db0f03c18ee5d
johnkirtley <kirtleyj16@gmail.com>
2020-07-17 10:56:00 -0700

    :sparkles: Comparing Execution Times of BST and Nested For Loop

--- names/names.py
+++ names/names.py
@@ -11,14 +11,20 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-# Return the list of duplicates in this data structure
 duplicates = []
 
-# Replace the nested for loops below with your improvements
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
+
+end_time = time.time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+
+start2 = time.time()
+
+duplicates2 = []
 
 search = BSTNode(names_1[0])
 
@@ -27,12 +33,12 @@ for n1 in names_1:
 
 for n2 in names_2:
     if search.contains(n2):
-        duplicates.append(n2)
+        duplicates2.append(n2)
 
 
-end_time = time.time()
+end2 = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print(f"runtime: {end_time - start_time} seconds")
+print(f"runtime: {end2 - start2} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
